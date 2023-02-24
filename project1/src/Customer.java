779bb16ab9d3c6fcc5b624d08e3716b5ee38e8a2
chewiethesmugg <chewantha@gmail.com>
2023-02-24 13:13:49 -0500

    messafw

--- project1/src/Customer.java
+++ project1/src/Customer.java
@@ -3,21 +3,18 @@ public class Customer {
 	//0:departure
 	public int type;
 	public double arrivalTime;
-	public double endTime;
 	public long customerId;
 	public boolean finished; //used to keep track if an event has already had its turn on the future events list
 	
 	public Customer(int id){
 		this.type=-1;
 		this.arrivalTime=-0.0;
-		this.endTime=-0.0;
 		this.customerId=(long)id;
 		this.finished=false;
 	}
 	public Customer(long id,double arr,int t) {
 		this.type=t;
 		this.arrivalTime=arr;
-		this.endTime=-0.0;
 		this.customerId=(long)id;
 		this.finished=false;
 	}
